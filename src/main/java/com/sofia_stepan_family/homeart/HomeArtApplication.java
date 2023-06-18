ba85ad4195c111c357d80bb9c52cfca59870fd4f
Stepan Omeliuk <stepan.omeliukh.knm.2019@lpnu.ua>
2023-06-19 00:17:14 +0300

    Creating a project

---
+++ src/main/java/com/sofia_stepan_family/homeart/HomeArtApplication.java
@@ -0,0 +1,13 @@
+package com.sofia_stepan_family.homeart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeArtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofia_stepan_family/homeart/HomeArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofia_stepan_family.homeart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
